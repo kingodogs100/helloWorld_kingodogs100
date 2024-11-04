f32f2d68943bd6c19869343095ca1cdb08046423
mayabenson <mayabenson@localhost>
2024-11-04 12:48:56 -0500

    becaususe
    because

--- helloWorld.groovy
+++ helloWorld.groovy
@@ -1,3 +1,3 @@
 import com.neuronrobotics.bowlerstudio.BowlerKernel
 // Have BowlerStudio speak messages
-BowlerKernel.speak("welcome to bowler studio you poophhhhhhhhhhhhhhhhhhhhhhhhhhaaaaaaaaaaaaaaaaaaaaaaaaaaaaeeeeeeeeeeeeeeeeee")
+BowlerKernel.speak("will aet a computer        you poophhhhhhhhhhhhhhhhhhhhhhhhhhaaaaaaaaaaaaaaaaaaaaaaaaaaaaeeeeeeeeeeeeeeeeeechichichicjhchichihahahahahahaha will aet a computer ")
